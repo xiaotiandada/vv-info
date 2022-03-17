5c4e294e3fd8a3d988048c5bab2e5b1364d82190
xiaotiandada <istianlei@qq.com>
2022-03-17 13:00:48 +0800

    feat: chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo} from 'react';
+import React, {useMemo, useState} from 'react';
 import TokenTable from './components/tokens/TokenTable';
 import {useTopTokenAddresses} from './data/tokens/topTokens';
 import {useFetchedTokenDatas} from './data/tokens/tokenData';
@@ -6,8 +6,45 @@ import {notEmpty} from './utils';
 import {TOKEN_HIDE} from './constants';
 import LineChart from './components/LineChart/alt';
 import {EthereumNetworkInfo} from './constants/networks';
+import {formatDollarAmount} from './utils/numbers';
 
-function App() {
+function UniswapOverview() {
+	// eslint-disable-next-line no-unused-vars
+	const [volumeHover, setVolumeHover] = useState<number | undefined>();
+
+	const [liquidityHover, setLiquidityHover] = useState<number | undefined>();
+	const [leftLabel, setLeftLabel] = useState<string | undefined>();
+	// eslint-disable-next-line no-unused-vars
+	const [rightLabel, setRightLabel] = useState<string | undefined>();
+
+	return (
+		<>
+			<h3 className="font-bold">Uniswap Overview</h3>
+			<LineChart
+				height={220}
+				minHeight={332}
+				color={EthereumNetworkInfo.primaryColor}
+				value={liquidityHover}
+				label={leftLabel}
+				setValue={setLiquidityHover}
+				setLabel={setLeftLabel}
+				topLeft={
+					<div>
+						<span>TVL</span>
+						<div>
+							<span className="tabular-nums">{formatDollarAmount(liquidityHover, 2, true)} </span>
+						</div>
+						<div>
+							{leftLabel ? <span className="tabular-nums">{leftLabel} (UTC)</span> : null}
+						</div>
+					</div>
+				}
+			/>
+		</>
+	);
+}
+
+function TopTokens() {
 	const {addresses: allTokenData} = useTopTokenAddresses();
 	const {data: allTokens} = useFetchedTokenDatas(allTokenData || []);
 	console.log('allTokens', allTokens);
@@ -18,12 +55,18 @@ function App() {
 		.filter(t => !TOKEN_HIDE.includes(t.address)), [allTokens]);
 
 	return (
-		<div>
-			<h3 className="font-bold">Uniswap Overview</h3>
-			<LineChart color={EthereumNetworkInfo.primaryColor} />
-
+		<>
 			<h3 className="font-bold">Top Tokens</h3>
 			<TokenTable tokenDatas={formattedTokens} />
+		</>
+	);
+}
+
+function App() {
+	return (
+		<div>
+			<UniswapOverview />
+			<TopTokens />
 			<h3 className="font-bold">Top Pools</h3>
 			{/* <TokenTable /> */}
 		</div>
--- src/components/LineChart/alt.tsx
+++ src/components/LineChart/alt.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, {FC, ReactNode, Dispatch, SetStateAction} from 'react';
 import {AreaChart, Area, XAxis, Tooltip, ResponsiveContainer} from 'recharts';
 import dayjs from 'dayjs';
 import {data} from './data';
@@ -7,12 +7,26 @@ import {darken} from 'polished';
 export type LineChartProps = {
   data?: any[]
   color?: string | undefined
+  height?: number | undefined
+  minHeight?: number
+  setValue?: Dispatch<SetStateAction<number | undefined>> // Used for value on hover
+  setLabel?: Dispatch<SetStateAction<string | undefined>> // Used for label of valye
+  value?: number
+  label?: string
+  topLeft?: ReactNode | undefined
+  topRight?: ReactNode | undefined
+  bottomLeft?: ReactNode | undefined
+  bottomRight?: ReactNode | undefined
 } & React.HTMLAttributes<HTMLDivElement>
 
 const Chart: FC<LineChartProps> = ({
 	color = '#56B2A4',
+	topLeft,
+	topRight,
 }) => (
 	<div className="w-[500px] h-[300px] bg-[#191B1F] rounded-[16px]">
+		{topLeft ?? null}
+		{topRight ?? null}
 		<ResponsiveContainer width="100%" height="100%">
 			<AreaChart
 				width={500}
